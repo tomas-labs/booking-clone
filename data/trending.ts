019cd41c804ce1cace92eb28b923297a71b6e226
tomasoliveira21 <tomasoliveira1018@gmail.com>
2024-08-11 15:20:16 +0100

    feat: landing page

---
+++ data/trending.ts
@@ -0,0 +1,37 @@
+export const trending_data = [
+    {
+      id: 1,
+      src: "https://shorturl.at/zjUwd",
+      title: "Dubai",
+      location: "United Arab Emirates",
+      description: "15 Deals",
+    },
+    {
+      id: 2,
+      src: "https://shorturl.at/mgqeK",
+      title: "South Korea",
+      location: "Asia",
+      description: "32 Deals",
+    },
+    {
+      id: 3,
+      src: "https://shorturl.at/o8hh7",
+      title: "London",
+      location: "United Kingdom",
+      description: "45 Deals",
+    },
+    {
+      id: 4,
+      src: "https://shorturl.at/CmbiZ",
+      title: "Singapore",
+      location: "Asia",
+      description: "88 Deals",
+    },
+    {
+      id: 5,
+      src: "https://rb.gy/fqlk72",
+      title: "New York City",
+      location: "United States of America",
+      description: "92 Deals",
+    },
+  ];
